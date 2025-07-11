6111c8940438e3a5e6813e3e7c095861c9ea98c7
skyline-nju <duanyu.nju@hotmail.com>
2025-07-11 21:41:05 +0800

    output snapshot with marked defects

--- scripts/detect_defects.py
+++ scripts/detect_defects.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from gsd import fl
+import os
+import glob
 
 
 def detect_defects(theta):
@@ -30,24 +32,42 @@ def detect_defects(theta):
                 y.append(j + 0.5)
     return x, y, charge
 
-if __name__ == "__main__":
 
-    folder = "/mnt/sda/LatticeXY/KM/L4096"
-    fname = f"{folder}/L4096_4096_T0.1_s0.2_h0.1_S3000.gsd"
+def show_defects(L, sigma, T, seed, beg_frame=-3, savefig=False):
+    if L == 4096:
+        folder = f"/mnt/sda/LatticeXY/KM/L{L:d}"
+    else:
+        folder = "/mnt/sda/LatticeXY/KM/snap"
+    fname = f"{folder}/L{L}_{L}_T{T:g}_s{sigma:g}_h0.1_S{seed:d}.gsd"
+    if savefig == True:
+        outdir = f"{folder}/{os.path.basename(fname).rstrip(".gsd")}"
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
     with fl.open(name=fname, mode="r") as fin:
         nframes = fin.nframes
         print("found", nframes, "frames")
         box = fin.read_chunk(frame=0, name="configuration/box")
         Lx, Ly = int(box[0]), int(box[1])
         extent = [-0.5, Lx-0.5, -0.5, Ly-0.5]
-                
-        for i_frame in range(nframes-2, nframes):
+        
+        if beg_frame is None:
+            snaps = glob.glob(f"{outdir}/*.png")
+            beg_frame = len(snaps)
+        elif beg_frame < 0:
+            beg_frame += nframes
+        
+        for i_frame in range(beg_frame, nframes):
+            print("frame", i_frame)
             theta = fin.read_chunk(frame=i_frame, name="particles/charge").reshape(Ly, Lx)
-
+            try:
+                t = fin.read_chunk(frame=i_frame, name="configuration/step")[0] * 0.1
+            except KeyError:
+                t = 0
             x, y, charge = detect_defects(theta)
 
-            fig, ax = plt.subplots(1, 1, figsize=(10, 10), constrained_layout=True)
-            ax.imshow(theta, cmap="hsv", origin="lower", vmin=-np.pi, vmax=np.pi, extent=extent)
+            fig, ax = plt.subplots(1, 1, figsize=(11, 10), constrained_layout=True)
+            theta[theta < 0] += np.pi * 2
+            im = ax.imshow(theta, cmap="hsv", origin="lower", vmin=0, vmax=2 * np.pi, extent=extent)
 
             for j in range(len(charge)):
                 if charge[j] == 1:
@@ -63,7 +83,16 @@ if __name__ == "__main__":
                 n_mins = np.sum(charge == -1)
             else:
                 n_tot = n_plus = n_mins = 0
-            ax.set_title(r"Defect number= %d with $n_{+1}=%d, n_{-1}=%g$" % (n_tot, n_plus, n_mins))
+            ax.set_title(r"Defect number= %d with $n_{+1}=%d, n_{-1}=%g, t=%g$" % (n_tot, n_plus, n_mins, t), fontsize="xx-large")
+            plt.colorbar(im)
 
-            plt.show()
+            if savefig:
+                plt.savefig(f"{outdir}/{i_frame:06d}.png")
+            else:
+                plt.show()
             plt.close()
+
+
+if __name__ == "__main__":
+    show_defects(L=1024, sigma=0.23, T=0.1, seed=3002, beg_frame=None, savefig=True)
+    
